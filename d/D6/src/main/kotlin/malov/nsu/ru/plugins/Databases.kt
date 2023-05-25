aa36e2cfa8f412bc3f62375fc058a328404079de
L3XxXa <alexkkk@inbox.ru>
2023-05-25 17:43:05 +0700

    Catching errors in query params and sending 400

--- d/D6/src/main/kotlin/malov/nsu/ru/plugins/Databases.kt
+++ d/D6/src/main/kotlin/malov/nsu/ru/plugins/Databases.kt
@@ -9,7 +9,6 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureDatabases() {
-
     val dbConnection: Connection = connectToPostgres(embedded = true)
 }
 
@@ -37,7 +36,7 @@ fun Application.configureDatabases() {
 fun Application.connectToPostgres(embedded: Boolean): Connection {
     Class.forName("org.postgresql.Driver")
     if (embedded) {
-        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo", "postgres", "admin")
     } else {
         val url = environment.config.property("postgres.url").getString()
         val user = environment.config.property("postgres.user").getString()
@@ -46,3 +45,5 @@ fun Application.connectToPostgres(embedded: Boolean): Connection {
         return DriverManager.getConnection(url, user, password)
     }
 }
+
+
--- d/D6/src/main/kotlin/malov/nsu/ru/plugins/Routing.kt
+++ d/D6/src/main/kotlin/malov/nsu/ru/plugins/Routing.kt
@@ -13,34 +13,59 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+
         get("/api/v1/airports"){
             call.respondText("List of all airports")
         }
+
         get("/api/v1/cities"){
             call.respondText("List of all cities")
         }
+
         get("/api/v1/airportsCity"){
+            if (call.request.queryParameters["city"] == null || call.request.queryParameters["city"] == ""){
+                call.respond(HttpStatusCode.BadRequest, "You didn't specified city to find")
+            }
             call.respondText(call.request.queryParameters["city"]!!)
         }
+
         get("/api/v1/airportInboundSchedule"){
+            if (call.request.queryParameters["airport"] == null || call.request.queryParameters["airport"] == ""){
+                call.respond(HttpStatusCode.BadRequest, "You didn't specified airport")
+            }
             call.respondText(call.request.queryParameters["airport"]!!)
         }
+
         get("/api/v1/airportOutboundSchedule"){
+            if (call.request.queryParameters["airport"] == null || call.request.queryParameters["airport"] == ""){
+                call.respond(HttpStatusCode.BadRequest, "You didn't specified airport")
+            }
             call.respondText(call.request.queryParameters["airport"]!!)
         }
+
         get("/api/v1/routes") {
+            if (call.request.queryParameters["origin"] == null || call.request.queryParameters["origin"] == "" ||
+                call.request.queryParameters["destination"] == null || call.request.queryParameters["destination"] == "" ||
+                call.request.queryParameters["time_from"] == null || call.request.queryParameters["time_from"] == "" ||
+                call.request.queryParameters["time_to"] == null || call.request.queryParameters["time_to"] == "" ||
+                call.request.queryParameters["booking_class"] == null || call.request.queryParameters["booking_class"] == "" ||
+                call.request.queryParameters["connections"] == null || call.request.queryParameters["connections"] == ""
+                ){
+                call.respond(HttpStatusCode.BadRequest, "You didn't specified all params")
+            }
             call.respondText(call.request.queryParameters["origin"]!! +
                     call.request.queryParameters["destination"]!! +
                     call.request.queryParameters["time_from"]!! +
                     call.request.queryParameters["time_to"]!! +
                     call.request.queryParameters["booking_class"]!! +
                     call.request.queryParameters["connections"]!!)
-
         }
+
         put("/api/v1/book"){
             val request = call.receive<BookRequestSerializer>()
             call.respond(HttpStatusCode.OK, request)
         }
+
         put("/api/v1/checkin"){
             val request = call.receive<CheckinRequestSerializer>()
             call.respond(HttpStatusCode.OK, request)
